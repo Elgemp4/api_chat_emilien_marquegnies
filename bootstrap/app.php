408f3295d62bb0ddb0beae44ad9cf2a3e226ee55
elgemp4 <marquegniesemilien@gmail.com>
2024-11-05 16:27:42 +0100

    Ajout de json only

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\EnforceJSON;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(EnforceJSON::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(NotFoundHttpException $e, Request $request) {
+            return response(null, 404);
+        });
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\SenderController;
 use App\Http\Middleware\CheckApiToken;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+Route::get("/", function() {
+    return response()->json((["message" => "This is an api to manage messages and senders, here are the differents routes : /content/item/messages /content/item/senders"]));
+});
 
 Route::get("/content/item/messages", [MessageController::class, "getMessages"]);
 Route::middleware(CheckApiToken::class)->post("/content/item/messages", [MessageController::class, "sendMessage"]);
